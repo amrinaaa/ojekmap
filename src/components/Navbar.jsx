8d6cff0697f14415638f2f2033a3427bf99652a5
amrina rossada <amrina0612@gmail.com>
2024-09-01 14:08:49 +0800

    upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 right-0 w-full bg-white text-black shadow-md">
+      <div className="bg-gradient-to-r from-primary to-secondary text-white">
+        <div className="container py-2">
+          <div className="flex justify-between items-center">
+            <h2 className="text-2xl font-bold">Ojek Map</h2>
+            {/* Elemen lainnya, seperti Link atau NavLink */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
